9523c880e9ba356c34a45e0428f2288655c6cf31
Ingrid <ingrihoegset@gmail.com>
2021-08-02 12:11:56 +0200

    Fikset problemet med manglende nedtellingslyd.

--- Runner/ViewModels/FirstGateViewModel.swift
+++ Runner/ViewModels/FirstGateViewModel.swift
@@ -110,6 +110,12 @@ class FirstGateViewModel: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate
         do {
             try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
             try AVAudioSession.sharedInstance().setActive(true)
+            
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
 
             guard let audioPlayer = audioPlayer else { return }
 
